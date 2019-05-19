633f43d005916b2791e9014a73109c127406464c
luis eduardo canedo torres <luillys83@yahoo.es>
2019-05-18 21:29:30 -0500

    Update index.php
    actualizando el index

--- index.php
+++ index.php
@@ -7,10 +7,33 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+	<!-- estilos -->
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
 </head>
 <body>
-
 	
+	<main>
+		<div class="container text-center">
+			<h1 class="py-4 bg-dark text-light rounded"><i class="fa fa-swatchbook"></i> Libreria</h1>
+			<div class="d-flex justify-content-center">
+				<form action="" method="post" accept-charset="utf-8" class="w-50">
+					<div class="py-2">
+						<div class="input-group mb-2">
+							<div class="input-group-prepend">
+								<div class="input-group-text bg-warning"><i class="fa fa-id-badge"></i></div>
+							</div>
+							<input type="text" autocomplete="off" class="form-control" name="" id="idnombre" placeholder="ID">
+						</div>
+					</div>
+					
+				</form>
+			</div>
+
+
+		</div>
+	</main>
+
 	
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
